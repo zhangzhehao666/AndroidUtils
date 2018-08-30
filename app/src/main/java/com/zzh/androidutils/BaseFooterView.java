84439e3f2e2954a72d0922307bc78b90258b6cb4
张哲豪 <zhangzhehao@tinypace.com>
2018-08-30 11:58:57 +0800

    Initial commit

---
+++ app/src/main/java/com/zzh/androidutils/BaseFooterView.java
@@ -0,0 +1,20 @@
+package com.zzh.androidutils;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+public abstract class BaseFooterView extends FrameLayout implements FooterViewListener{
+
+    public BaseFooterView(Context context) {
+        super(context);
+    }
+
+    public BaseFooterView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BaseFooterView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
---
+++ app/src/main/java/com/zzh/androidutils/FooterViewListener.java
@@ -0,0 +1,24 @@
+package com.zzh.androidutils;
+
+public interface FooterViewListener {
+
+    /**
+     * 网络不好的时候想要展示的UI
+     */
+    void onNetChange(boolean isAvailable);
+
+    /**
+     * 正常的loading的View
+     */
+    void onLoadingMore();
+
+    /**
+     * 没有更多数据
+     */
+    void onNoMore(CharSequence message);
+
+    /**
+     *  错误时展示的View
+     */
+    void onError(CharSequence message);
+}
